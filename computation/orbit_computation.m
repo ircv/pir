0584985855f3dee902615c92c1306192dc3b882e
ircv <38881307+ircv@users.noreply.github.com>
2018-05-01 12:41:13 +0200

    Update orbit_computation.m

--- computation/orbit_computation.m
+++ computation/orbit_computation.m
@@ -1,5 +1,5 @@
 function orbit = orbit_computation(cr3bp, orbit, params, cst)
-% ORBIT_COMPUTATION generic routine for the computation of halo, vertical
+% ORBIT_COMPUTATION generic routine for the computation of halo, nro, vertical
 % and planar lyapunov symmetric periodic orbits in the CRTBP.
 %
 % ORBIT = ORBIT_COMPUTATION(CR3BP, ORBIT, PARAMS, CST) is the default call
@@ -41,19 +41,81 @@ function orbit = orbit_computation(cr3bp, orbit, params, cst)
 %
 % BLB 2016
 
-%--------------------------------------------------------------------------
-% Initialization from third order Richardson approximation.
-% The initial conditions are taken on the plane y = 0, at t = 0.0
-%--------------------------------------------------------------------------
-yvg   =  third_order_orbit(orbit, 0.0, cst);
-fprintf('first guess found\n');
+if ( strcmp(orbit.type,cst.orbit.type.PLYAP) || (~strcmp(orbit.type,cst.orbit.type.PLYAP) && orbit.Azdim<=30000) ) 
+    %----------------------------------------------------------------------
+    % Initialization from third order Richardson approximation.
+    % The initial conditions are taken on the plane y = 0, at t = 0.0
+    %----------------------------------------------------------------------
+    yvg   =  third_order_orbit(orbit, 0.0, cst);
+    % ---------------------------------------------------------------------
+    % Computation with default parameters. This step makes use of a
+    % differential correction process.
+    %----------------------------------------------------------------------
+    orbit = orbit_refinement(cr3bp, orbit, params, yvg, cst);
 
-%--------------------------------------------------------------------------
-% Computation with default parameters. This step makes use of a
-% differential correction process.
-%--------------------------------------------------------------------------
-orbit = orbit_refinement(cr3bp, orbit, params, yvg, cst);
-fprintf('differential correction done\n');
+    else         
+        %------------------------------------------------------------------
+        % First computation: a true orbit is computed with a simple 
+        % 3-dimensionnal differential corrector scheme 
+        % (no pseudo-arclength). The z component of the initial state is 
+        % fixed.
+        %------------------------------------------------------------------
+        if ( orbit.li.number == 1 )
+            work_orbit= init_orbit(cr3bp, cr3bp.l1, orbit.type, cst.orbit.family.NORTHERN,2000,cst);   
+        else
+            work_orbit = init_orbit(cr3bp,cr3bp.l2, orbit.type, cst.orbit.family.NORTHERN,8000,cst);
+        end       
+        % Initialization from third order approximation
+        yvg =  third_order_orbit(work_orbit, 0.0, cst);
+        % Computation with default parameters
+        work_orbit = orbit_refinement(cr3bp, work_orbit, params, yvg, cst, cst.corr.Z0_FIXED);
+        work_orbit = orbit_postprocess(cr3bp,work_orbit,params,cst);
+        %------------------------------------------------------------------
+        % Second computation: initialized by the previous computation, a 
+        % new orbit is obtained with a 4-dimensional differential corrector.
+        % The z component is now part of the free variables. The objects 
+        % necessary to go on with the continuation are initialized in orbit.
+        % cont (free-variables vector,null vector of the Jacobian).
+        %------------------------------------------------------------------
+        work_orbit = orbit_refinement(cr3bp, work_orbit, params, work_orbit.y0, cst, cst.corr.MIN_NORM);
+        work_orbit = orbit_postprocess(cr3bp,work_orbit,params,cst);
+        % Arclength stepsize
+        work_orbit.cont.ds = 0.01;
+        % Loop
+        datas.initialconditions(1,:)=work_orbit.y0(1:6)';
+        datas.Az(1)=work_orbit.Azdim;
+        i=2;stop=0;
+        fprintf ('orbit computation in progress...');
+        while (i>=stop)
+            work_orbit = orbit_refinement_cont(cr3bp, work_orbit, params, cst);
+            datas.Az(i)=work_orbit.Azdim;
+            datas.initialconditions(i,:)=work_orbit.y0(1:6)';
+            if ( (orbit.Azdim<datas.Az(i) && orbit.Azdim>datas.Az(i-1)) || (orbit.Azdim>datas.Az(i) && orbit.Azdim<datas.Az(i-1)) )
+                if ( strcmp(orbit.subtype,cst.orbit.subtype.NRO) )
+                    pos = work_orbit.perigee.position;
+                    if( abs(pos(1) - cr3bp.m2.pos(1)) < cr3bp.m2.Rm/cr3bp.L && work_orbit.perigee.altitude > 0 )
+                        stop=i+6;
+                    end
+                else
+                    stop=i+6;
+                end
+            end
+            i=i+1;
+         end
+         fit = interpol(datas, datas.Az, orbit.Azdim);
+        %------------------------------------------------------------------
+        % Third Computation: Interpolation results
+        %------------------------------------------------------------------
+        yv0 = fit.f(1:6);
+        if( strcmp(orbit.family,cst.orbit.family.SOUTHERN) )
+            yv0(3) = -yv0(3);
+        end
+        %------------------------------------------------------------------
+        % Fourth Computation: differential correction
+        %------------------------------------------------------------------
+        orbit = orbit_refinement(cr3bp, orbit, params, yv0, cst);
+end
+
 %--------------------------------------------------------------------------
 % Postprocess. After this step, the following elements are updated in the
 % orbit structure:
@@ -74,12 +136,29 @@ fprintf('differential correction done\n');
 %   matrix.
 %--------------------------------------------------------------------------
 orbit = orbit_postprocess(cr3bp, orbit,params,cst);
-fprintf('orbit built\n');
 % --------------------------------------------------------------------------
 % Plotting (potentially)
 % --------------------------------------------------------------------------
 if(params.plot.orbit) %plotting
     orbit_plot(orbit, params);
+end
+
+end
+
+function fit = interpol(abacus, data, value)
+
+fit.half = 2;
+[~, array_position] = min(abs(data - value));
+mini = max(array_position - fit.half, 1);
+maxi = min(array_position + fit.half, length(data));
+fit.degree = length(mini:maxi)-1;
+fit.x =  data(mini:maxi)';
+for count =1:6
+    fit.y(:,count) =  abacus.initialconditions(mini:maxi,count);
+    %Fitting for every dimension of the state (6)
+    [fit.p, ~, fit.mu] = polyfit(fit.x,fit.y(:,count),fit.degree);
+    %Evaluation
+    fit.f(count) = polyval(fit.p,value,[],fit.mu);
 end
 
 end
